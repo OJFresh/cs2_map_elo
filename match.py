190eb30f2e4ce4970a43a3aecf76e346ff4dc3a3
Oliver Ryan <o.jryan98@gmail.com>
2024-11-23 13:29:06 +0000

    pushing match information

--- match.py
+++ match.py
@@ -2,10 +2,11 @@
 
 class Match:
 
-    def __init__(self, date, team_a, team_b, rounds_a, rounds_b):
+    def __init__(self, date, map_name, team_a, team_b, rounds_a, rounds_b):
         self.date = date
         self.team_a = team_a
         self.team_b = team_b
+        self.map_name = map_name
         self.rounds_a = rounds_a
         self.rounds_b = rounds_b
 
