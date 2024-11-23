6dadfd22bc4f4c51d0da2105458df3579cc4627f
Oliver Ryan <o.jryan98@gmail.com>
2024-11-23 14:06:54 +0000

    elo calculation

--- match.py
+++ match.py
@@ -1,3 +1,4 @@
+from team import Team
 
 
 class Match:
@@ -10,6 +11,32 @@ class Match:
         self.rounds_a = rounds_a
         self.rounds_b = rounds_b
 
+    def modify_elo(self):
+        diff = 16 - (self.winner.elo - self.loser.elo)/800
+        self.winner.elo += diff
+        self.loser.elo -= diff
+        print(f'{self.winner} elo is now {self.winner.elo}')
+        print(f'{self.loser} elo is now {self.loser.elo}')
+
+
     @property
     def winner(self):
         return self.team_a if self.rounds_a > self.rounds_b else self.team_b
+
+    @property
+    def loser(self):
+        return self.team_a if self.rounds_a < self.rounds_b else self.team_b
+
+vitality = Team()
+mouz = Team()
+navi = Team()
+spirit = Team()
+g2 = Team()
+
+print(vitality.elo)
+print(g2.elo)
+
+major_final = Match(date='1234', team_a=vitality, team_b=g2, rounds_a=11, rounds_b=13, map_name='mirage')
+major_final.modify_elo()
+major_final2 = Match(date='1234', team_a=vitality, team_b=g2, rounds_a=11, rounds_b=13, map_name='mirage')
+major_final2.modify_elo()
