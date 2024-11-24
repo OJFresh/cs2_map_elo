7068bfa25643149edc55a631b65d8e7fb00a123b
Oliver Ryan <o.jryan98@gmail.com>
2024-11-24 02:35:25 +0000

    elo calculation

--- analysis.py
+++ analysis.py
@@ -1,13 +1,16 @@
 import pandas as pd
 
-from match import Match
-
 
 def execute():
-    elo = pd.read_csv('config/elo.csv')
-    for i, x in elo.iterrows():
-        kwargs = x.to_dict()
-        print(kwargs)
+    map_ranking('Mirage')
+
+
+def map_ranking(map_name):
+    elo = pd.read_csv('config/elo.csv', index_col='team_name')
+    map_df = elo[map_name]
+    sorted_ranking = elo.sort_values(map_name, ascending=False)
+    for i, (team, df_dict) in enumerate(sorted_ranking.iterrows()):
+        print(f'{i+1}) {team} ({df_dict[map_name]})')
 
 
 execute()
